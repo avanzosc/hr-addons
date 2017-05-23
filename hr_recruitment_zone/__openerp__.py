avanzosc/odoo-addons

[ADD] new module hr_recruitment_zone

hr_recruitment_zone/__openerp__.py
@@ -1,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2016 Esther Martín - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Hr Recruitment Zone",
+    "version": "8.0.1.0.0",
+    "category": "Custom Module",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Esther Martín <esthermartin@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "depends": [
+        "hr_recruitment",
+        "partner_zone",
+    ],
+    "data": [
+        "views/hr_view.xml",
+    ],
+    "installable": True,
+}

avanzosc/hr-addons

hr_recruitment_zone/__openerp__.py
@@ -1,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2016 Esther Martín - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Hr Recruitment Zone",
+    "version": "8.0.1.0.0",
+    "category": "Custom Module",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Esther Martín <esthermartin@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "depends": [
+        "hr_recruitment",
+        "partner_zone",
+    ],
+    "data": [
+        "views/hr_view.xml",
+    ],
+    "installable": False,
+}