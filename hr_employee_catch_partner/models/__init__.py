f4e48cc77890c6b1960cd691218e753c08d16d75
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2018-01-15 10:44:30 +0100

    [MIG] hr_employee_catch_partner: Migration to 11.0

--- hr_employee_catch_partner/models/__init__.py
+++ hr_employee_catch_partner/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# (c) 2016 Alfredo de la Fuente - AvanzOSC
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# Copyright (c) 2017 Alfredo de la fuente <alfredodelafuente@avanzosc.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from . import hr_employee
 from . import res_partner
--- hr_employee_catch_partner/tests/test_hr_employee_catch_partner.py
+++ hr_employee_catch_partner/tests/test_hr_employee_catch_partner.py
@@ -1,31 +1,26 @@
 # -*- coding: utf-8 -*-
-# (c) 2016 Alfredo de la Fuente - AvanzOSC
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-import openerp.tests.common as common
+# Copyright (c) 2017 Alfredo de la fuente <alfredodelafuente@avanzosc.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo.tests import common
 
 
 class TestHrEmployeeCatchPartner(common.TransactionCase):
 
     def setUp(self):
         super(TestHrEmployeeCatchPartner, self).setUp()
-        self.employee_model = self.env['hr.employee']
-        self.demo_user = self.ref('base.user_demo')
-        employee_vals = {'name': 'employee name',
-                         'user_id':  self.ref('base.user_root')}
-        self.employee = self.employee_model.create(employee_vals)
+        self.user = self.browse_ref('base.user_root')
+        self.partner = self.browse_ref('base.res_partner_10')
 
     def test_hr_employee_catch_partner(self):
-        result = self.employee.onchange_user(self.ref('base.user_root'))
-        self.assertNotEqual(
-            result.get('value', False), False, 'Value not found in result')
-        self.assertNotEqual(
-            result['value'].get('address_home_id', False), False,
-            'Partner not found')
-
-    def test_create_with_addres_home(self):
-        employee = self.employee_model.create({
-            'name': 'Test employee',
-            'user_id': self.demo_user,
-            'address_home_id': self.demo_user,
-        })
-        self.assertEqual(employee.address_home_id.employee_id.id, employee.id)
+        employee_vals = {'name': 'employee name',
+                         'address_home_id': self.user.partner_id.id}
+        self.employee = self.env['hr.employee'].create(employee_vals)
+        self.employee.user_id = self.user
+        self.employee._onchange_user()
+        self.assertEqual(
+            self.employee.address_home_id, self.user.partner_id,
+            'Employee without partner')
+        self.employee.address_home_id = self.partner.id
+        self.assertEqual(
+            self.partner.employee_id, self.employee,
+            'Bad modification for employee 1')
