e7f8ea6d086719cb501ca6c61bf9369aef3ea40a
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2018-01-22 15:27:51 +0100

    [ADD] hr_employee_product: When generating analytical line from time imputation, bring the product to the analytical line.

---
+++ hr_employee_product/README.rst
@@ -0,0 +1,39 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: https://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+===================
+Hr employee product
+===================
+
+ * This module allows you to define a product in the employee's form view.
+ * When an analytical line is generated from time imputation, bring the
+   following fields to the analytical line: product of the employee, product
+   category, employee, department of the employee.
+
+Configuration
+=============
+
+
+Usage
+=====
+
+.. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
+   :alt: Try me on Runbot
+   :target: https://runbot.odoo-community.org/runbot/134/11.0
+
+Bug Tracker
+===========
+
+
+Credits
+=======
+
+Images
+------
+
+Contributors
+------------
+
+* Ana Juaristi <ajuaristio@gmail.com>
+* Alfredo de la Fuente <alfredodelafuente@avanzosc.es>
---
+++ hr_employee_product/models/account_analytic_line.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018 Alfredo de la fuente <alfredodelafuente@avanzosc.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import models, fields, api
+
+
+class AccountAnalyticLine(models.Model):
+    _inherit = 'account.analytic.line'
+
+    product_categ_id = fields.Many2one(
+        comodel_name='product.category', string='Product category',
+        related='product_id.categ_id', store=True, readonly=True)
+
+    @api.model
+    def create(self, vals):
+        if vals.get('employee_id', False):
+            vals = self._catch_employee_information(vals)
+        return super(AccountAnalyticLine, self).create(vals)
+
+    @api.multi
+    def write(self, vals):
+        if vals.get('employee_id', False):
+            vals = self._catch_employee_information(vals)
+        return super(AccountAnalyticLine, self).write(vals)
+
+    def _catch_employee_information(self, vals):
+        employee = self.env['hr.employee'].browse(vals.get('employee_id'))
+        if employee.product_id:
+            vals['product_id'] = employee.product_id.id
+        return vals
