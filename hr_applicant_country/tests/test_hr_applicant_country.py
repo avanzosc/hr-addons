66aa20010d247e2563ef705aa4ed19c1eeaf73b8
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2018-03-13 14:32:55 +0100

    [ADD] hr_applicant_country: Show country in "Job positions" and "Applications" objects.

---
+++ hr_applicant_country/tests/test_hr_applicant_country.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# (Copyright) 2018 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+import openerp.tests.common as common
+
+
+class TestHrApplicantCountry(common.TransactionCase):
+
+    def setUp(self):
+        super(TestHrApplicantCountry, self).setUp()
+        self.country = self.env['res.country'].create(
+            {'name': 'Country for test hr applicant country'})
+        self.partner = self.env['res.partner'].create(
+            {'name': 'Partner for test hr applicant country',
+             'country_id': self.country.id})
+        self.application = self.env['hr.applicant'].create(
+            {'name': 'Applicant for test hr applicant country',
+             'partner_id': self.partner.id})
+
+    def test_hr_applicant_country(self):
+        res = self.application.onchange_partner_id(self.partner.id)
+        values = res.get('value')
+        country_id = values.get('contact_country_id')
+        self.assertEqual(
+            country_id, self.country.id, 'Bad country for application contact')
