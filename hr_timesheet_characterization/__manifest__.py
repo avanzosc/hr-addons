a4c24b84a4fcf6862e950189db4cc6d51aca1297
oihane <oihanecruce@gmail.com>
2018-05-28 18:01:52 +0200

    [ADD] new module hr_timesheet_characterization (#37)

---
+++ hr_timesheet_characterization/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2018 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Timesheets Characterization",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "hr_timesheet",
+        "project_characterization",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Human Resources",
+    "data": [
+        "views/account_analytic_line_view.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
