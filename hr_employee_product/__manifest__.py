2f216292a9ec8a53c194e096f82bb1b5371123f8
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-06-19 12:12:49 +0200

    [IMP] hr_employee_product: fix some views and some code cleaning

--- hr_employee_product/__manifest__.py
+++ hr_employee_product/__manifest__.py
@@ -1,26 +1,25 @@
-# -*- coding: utf-8 -*-
 # Copyright (c) 2018 Alfredo de la fuente <alfredodelafuente@avanzosc.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Hr Employee Product',
+    'name': 'Employee Product',
     'version': '11.0.1.0.0',
     'license': "AGPL-3",
-    'summary': '''Hr employee with product''',
+    'summary': '''Define a product for employee''',
     'author':  "AvanzOSC",
     'website': 'http://www.avanzosc.es',
     'contributors': [
         "Ana Juaristi <anajuaristi@avanzosc.es>",
-        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es",
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es>",
     ],
     'category': 'Human Resources',
     'depends': [
         'project',
         'hr_timesheet',
-        'account'
+        'account',
     ],
     'data': [
         "views/hr_employee_view.xml",
-        "views/account_analytic_line_view.xml"
+        "views/account_analytic_line_view.xml",
     ],
     'demo': [],
     'installable': True,
