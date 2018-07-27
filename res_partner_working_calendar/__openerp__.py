adf7c1cf7fdeeddd88dbef274182b6e0bf727c6d
eideroyarbide@avanzosc.es <eideroyarbide@avanzosc.es>
2018-07-27 11:40:29 +0200

    [ADD] <res_partner_working_calendar>: This module allows you to define the working calendar for partners

---
+++ res_partner_working_calendar/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Eider Oyarbide - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Res Partner Working Calendar",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+        "Eider Oyarbide <eideroyarbide@avanzosc.es",
+    ],
+    "category": "Human Resources",
+    "depends": [
+        "hr_contract",
+                ],
+    "data": [
+        "views/res_partner_view.xml"
+    ],
+    "installable": True,
+}
