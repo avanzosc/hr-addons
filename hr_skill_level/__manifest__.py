a0ff2efa8842afb0e359810a7128442b507a4bcf
Gontzal Gomez <gontzalgomez@avanzosc.es>
2018-12-04 11:47:10 +0100

    [ADD] hr_skill_level: New module created (#44)

---
+++ hr_skill_level/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2018 Gontzal Gomez - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Skill Level",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "hr",
+        "hr_skill",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Human Resources",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/hr_skill_level_view.xml",
+    ],
+    "installable": True,
+}
