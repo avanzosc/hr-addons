91ee0d6f6b0c92fda34f7b1b1caa9c620c459751
oihane <oihanecruce@gmail.com>
2019-10-16 10:07:14 +0200

    [ADD] New module hr_partner (#56)
    * [ADD] New module hr_partner
    
    * [IMP] hr_partner: add tests
    
    * [IMP] hr_partner: add translations

---
+++ hr_partner/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2018 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Employee Partner",
+    "version": "11.0.1.0.0",
+    "category": "Human Resources",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "hr",
+    ],
+    "data": [
+        "views/hr_employee_view.xml",
+    ],
+    "installable": True,
+}
