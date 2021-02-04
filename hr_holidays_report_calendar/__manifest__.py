b1f5ae63aeb7c3235d5828b780b8d0940c9bf4a1
Oihane Crucelaegui <oihanecruce@gmail.com>
2021-02-04 17:29:36 +0100

    [ADD] new module hr_holidays_report_calendar

---
+++ hr_holidays_report_calendar/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Leaves Calendar",
+    "version": "12.0.1.0.0",
+    "category": "Human Resources",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "hr_holidays",
+    ],
+    "excludes": [],
+    "data": [
+        "views/hr_leave_report_view.xml",
+    ],
+    "installable": True,
+}
