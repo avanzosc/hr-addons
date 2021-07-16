54715d0f2b600d6b442fc4bc9da0d6127197d2b8
Tu Nombre <you@example.com>
2021-07-16 13:00:17 +0200

    [ADD] hr_timesheet_usability: Onchange that calculates the duration of the timesheets.  A button in the tree view of the timesheets to put the end date.  Two buttons in tasks to initiate and end a timesheet.

---
+++ hr_timesheet_usability/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Berezi - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Hr Timesheet Usability",
+    'version': '12.0.1.0.0',
+    "author": "Avanzosc",
+    "website": "http://www.avanzosc.es",
+    "category": "Project",
+    "depends": [
+        "project",
+        "hr_timesheet",
+        "hr_timesheet_activity_begin_end",
+        "account"
+    ],
+    "data": [
+        "views/account_analytic_line_views.xml",
+        "views/project_task_views.xml"
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
