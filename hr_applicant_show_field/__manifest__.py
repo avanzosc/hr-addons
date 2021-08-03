deda2d31d949e3d0696ee868737e031c2de38a89
Thor <alfredodelafuente@avanzosc.es>
2021-08-03 09:51:35 +0200

    [ADD] hr_applicant_show_field: In "Applicant" object show fields "contact", and "employee".

---
+++ hr_applicant_show_field/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Hr Applicant Show Field",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Human Resources/Recruitment",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "hr_recruitment"
+    ],
+    "data": [
+        "views/hr_applicant_views.xml"
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
