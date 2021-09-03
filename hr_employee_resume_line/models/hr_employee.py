d828725e4b5af80289d02474be93b91d2f97ea28
Avanzosc <leiremartinez@avanzosc.es>
2021-09-03 13:04:11 +0200

    [IMP] hr_employee_resume_line: improve code for _compute_last_resume_lines

--- hr_employee_resume_line/models/hr_employee.py
+++ hr_employee_resume_line/models/hr_employee.py
@@ -16,9 +16,10 @@ class HrEmployee(models.Model):
             line_channels = record.resume_line_ids.mapped('channel_id')
             unique_last_resume_line_ids = []
             for channel in line_channels:
-                last_channel_record = record.resume_line_ids.filtered(
-                    lambda r: r.channel_id == channel).sorted('date_end')
-                unique_last_resume_line_ids.append(last_channel_record[0].id)
+                unique_last_resume_line = max(record.resume_line_ids.filtered(
+                    lambda r: r.channel_id == channel),
+                    key=lambda x: x.date_end)
+                unique_last_resume_line_ids.append(unique_last_resume_line.id)
 
             record.last_resume_line_ids = record.resume_line_ids.filtered(
                 lambda r: r.id in unique_last_resume_line_ids
