731904bb5d040f44f44760ec3df737bbd664d7d2
Thor <alfredodelafuente@avanzosc.es>
2021-09-30 11:08:48 +0200

    [ADD] hr_timesheet_sheet_without_expense: No show timesheet sheets with product expense.

---
+++ hr_timesheet_sheet_without_expense/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Hr Timesheet Sheet Without Expense",
+    'version': '14.0.1.0.0',
+    'author': 'AvanzOSC',
+    'website': 'http://www.avanzosc.es',
+    'category': 'Marketing/Events',
+    'license': 'AGPL-3',
+    'depends': [
+        'hr_expense',
+        'hr_timesheet_sheet',
+        'hr_timesheet_sheet_autodraft'
+    ],
+    'data': [
+    ]
+}
