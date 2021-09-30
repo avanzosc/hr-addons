16ec6e286cab679dc965316fb4e54c0a9bc15e75
Thor <alfredodelafuente@avanzosc.es>
2021-09-30 10:05:30 +0200

    [ADD] hr_expense_from_analytic: When an analytical entry is created with a product of the "expense" type, create an "Expense".

---
+++ hr_expense_from_analytic/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Hr Expense From Analytic",
+    'version': '14.0.1.0.0',
+    'author': 'AvanzOSC',
+    'website': 'http://www.avanzosc.es',
+    'category': 'Marketing/Events',
+    'license': 'AGPL-3',
+    'depends': [
+        'account',
+        'hr_expense',
+    ],
+    'data': [
+    ]
+}
