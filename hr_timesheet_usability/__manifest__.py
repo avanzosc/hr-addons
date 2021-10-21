a28cef82d7f36d4d6d50a7c055ffb2d0e41b67ab
Oihane Crucelaegui <oihanecruce@gmail.com>
2021-10-21 11:15:02 +0200

    [ADD] New moduloe hr_timesheet_usability

---
+++ hr_timesheet_usability/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Task Logs Usability",
+    "version": "14.0.1.0.0",
+    "category": "Services/Timesheets",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "hr_timesheet",
+    ],
+    "data": [],
+    "installable": True,
+}
