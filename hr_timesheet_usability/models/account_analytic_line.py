0467426b9ad88fa642ba6fb171d40faf5ef5793c
Thor <alfredodelafuente@avanzosc.es>
2022-01-24 15:34:58 +0100

    [IMP] hr_timesheet_usability: En lineas de analitica inicializar los campos hora inicio y fin, y también empleado.

--- hr_timesheet_usability/models/account_analytic_line.py
+++ hr_timesheet_usability/models/account_analytic_line.py
@@ -1,6 +1,6 @@
 # Copyright 2021 Berezi - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, api
+from odoo import models, api, fields
 from datetime import datetime
 import pytz
 from datetime import timedelta
@@ -8,6 +8,25 @@ from datetime import timedelta
 
 class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
+
+    def default_times(self):
+        timezone = pytz.timezone(self._context.get('tz') or 'UTC')
+        date = fields.Datetime.now()
+        date = date.replace(tzinfo=pytz.timezone(
+            'UTC')).astimezone(timezone)
+        hours = int(date.strftime("%H"))
+        minutes = int(date.strftime("%M"))
+        return round(hours + minutes/60, 14)
+
+    def default_employee_id(self):
+        employee_obj = self.env['hr.employee']
+        cond = [('user_id', '=', self.env.user.id)]
+        employee = employee_obj.search(cond, limit=1)
+        return employee if employee else employee_obj
+
+    time_start = fields.Float(default=default_times)
+    time_stop = fields.Float(default=default_times)
+    employee_id = fields.Many2one(default=default_employee_id)
 
     @api.multi
     def action_button_end(self):
