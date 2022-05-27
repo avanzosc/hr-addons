1cc97fa551ba8bb1d0cf572764a70188e39da202
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-05-27 11:00:49 +0200

    [IMP] hr_employee_resume_line: add depends

--- hr_employee_resume_line/models/hr_employee.py
+++ hr_employee_resume_line/models/hr_employee.py
@@ -1,24 +1,28 @@
-
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class HrEmployee(models.Model):
-    _inherit = 'hr.employee'
+    _inherit = "hr.employee"
 
     last_resume_line_ids = fields.One2many(
-        'hr.resume.line', 'employee_id', string="Last resumé lines",
+        comodel_name="hr.resume.line",
+        inverse_name="employee_id",
+        string="Last resumé lines",
         compute="_compute_last_resume_lines",
     )
 
+    @api.depends("resume_line_ids")
     def _compute_last_resume_lines(self):
         for record in self:
-            line_channels = record.resume_line_ids.mapped('channel_id')
+            line_channels = record.mapped("resume_line_ids.channel_id")
             unique_last_resume_line_ids = record.resume_line_ids.filtered(
-                lambda r: r.channel_id == False).ids
+                lambda r: not r.channel_id
+            ).ids
             for channel in line_channels:
-                unique_last_resume_line = max(record.resume_line_ids.filtered(
-                    lambda r: r.channel_id == channel),
-                    key=lambda x: x.date_end)
+                unique_last_resume_line = max(
+                    record.resume_line_ids.filtered(lambda r: r.channel_id == channel),
+                    key=lambda x: x.date_end,
+                )
                 unique_last_resume_line_ids.append(unique_last_resume_line.id)
 
             record.last_resume_line_ids = record.resume_line_ids.filtered(
