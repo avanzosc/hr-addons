42d45d5c15f780663f996c1046fef663bc635165
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-27 09:55:56 +0200

    [IMP] hr_applicant_show_field: pre-commit stuff

--- hr_applicant_show_field/__manifest__.py
+++ hr_applicant_show_field/__manifest__.py
@@ -2,16 +2,16 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Hr Applicant Show Field",
-    'version': '14.0.1.0.0',
+    "version": "14.0.1.0.0",
     "author": "Avanzosc",
     "category": "Human Resources/Recruitment",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/hr-addons",
     "depends": [
-        "hr_recruitment"
+        "hr_recruitment",
     ],
     "data": [
-        "views/hr_applicant_views.xml"
+        "views/hr_applicant_views.xml",
     ],
     "license": "AGPL-3",
-    'installable': True,
+    "installable": True,
 }
