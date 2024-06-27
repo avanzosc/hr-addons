b828853a78c3abf80a0c8fbe22e3889ad52b52fe
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-27 09:30:56 +0200

    [IMP] hr_timesheet_schedule: pre-commit stuff

--- hr_timesheet_schedule/__manifest__.py
+++ hr_timesheet_schedule/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Hr Timesheet Schedule",
     "version": "14.0.1.0.0",
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/hr-addons",
     "category": "Services/Timesheets",
     "license": "AGPL-3",
     "depends": [
