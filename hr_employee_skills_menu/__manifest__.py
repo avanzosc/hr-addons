43a8f9554f3c7676c430f4def2fdabb003741537
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-27 09:54:26 +0200

    [IMP] hr_employee_skills_menu: pre-commit stuff

--- hr_employee_skills_menu/__manifest__.py
+++ hr_employee_skills_menu/__manifest__.py
@@ -2,17 +2,17 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Hr Employee Skills Menu",
-    'version': '14.0.1.0.0',
+    "version": "14.0.1.0.0",
     "author": "Avanzosc",
-    "website": "https://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/hr-addons",
     "category": "Human Resources/Employees",
     "depends": [
         "hr_skills",
     ],
     "data": [
         "views/hr_employee_skill.xml",
-        "views/hr_employee.xml"
+        "views/hr_employee.xml",
     ],
     "license": "AGPL-3",
-    'installable': True,
+    "installable": True,
 }
