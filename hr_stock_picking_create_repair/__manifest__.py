43cbee68eb2b81c27ee44c79e2ee6031d84606c7
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-27 09:36:52 +0200

    [IMP] hr_stock_picking_create_repair: pre-commit stuff

--- hr_stock_picking_create_repair/__manifest__.py
+++ hr_stock_picking_create_repair/__manifest__.py
@@ -6,7 +6,7 @@
     "category": "Human Resources/Employees",
     "license": "AGPL-3",
     "author": "Avanzosc",
-    "website": "https://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/hr-addons",
     "depends": [
         "stock_picking_create_repair",
         "hr",
