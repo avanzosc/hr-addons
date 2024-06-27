d075f75b17f21e24cfc03381dd7aba9f5731d942
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-27 09:32:34 +0200

    [IMP] hr_timesheet_begin_end_usability: pre-commit stuff

--- hr_timesheet_begin_end_usability/__manifest__.py
+++ hr_timesheet_begin_end_usability/__manifest__.py
@@ -7,14 +7,14 @@
     "category": "Services/Timesheets",
     "license": "AGPL-3",
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/hr-addons",
     "depends": [
         "analytic",
         "hr_timesheet",
-        "hr_timesheet_activity_begin_end"
+        "hr_timesheet_activity_begin_end",
     ],
     "data": [
-        "views/account_analytic_line_view.xml"
+        "views/account_analytic_line_view.xml",
     ],
     "installable": True,
 }
