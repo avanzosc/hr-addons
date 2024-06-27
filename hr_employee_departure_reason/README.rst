65a35732d365bc7ec2177bbfe8ee1fddebc92244
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-27 09:34:25 +0200

    [IMP] hr_employee_departure_reason: pre-commit stuff

--- hr_employee_departure_reason/README.rst
+++ hr_employee_departure_reason/README.rst
@@ -2,9 +2,9 @@
     :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
     :alt: License: AGPL-3
 
-==========================
-Hr employee departure reason
-==========================
+=========================
+Employee departure reason
+=========================
 
 * New object employee departure reason in employee.
 
--- hr_employee_departure_reason/__manifest__.py
+++ hr_employee_departure_reason/__manifest__.py
@@ -1,10 +1,10 @@
 # Copyright 2021 Berezi - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Hr Employee Departure Reason",
-    'version': '14.0.1.0.0',
+    "name": "Employee departure reason",
+    "version": "14.0.1.0.0",
     "author": "Avanzosc",
-    "website": "https://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/hr-addons",
     "category": "Human Resources/Employees",
     "depends": [
         "hr",
@@ -13,8 +13,8 @@
         "security/ir.model.access.csv",
         "data/hr_employee_departure_reason.xml",
         "views/hr_employee_departure_reason.xml",
-        "wizard/hr_departure_wizard_views.xml"
+        "wizard/hr_departure_wizard_views.xml",
     ],
     "license": "AGPL-3",
-    'installable': True,
+    "installable": True,
 }
