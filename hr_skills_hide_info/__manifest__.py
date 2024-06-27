d74ff5d7b402b0ce07c23311e03cc1876fd3ac06
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-27 09:45:24 +0200

    [IMP] hr_skills_hide_info: pre-commit stuff

--- hr_skills_hide_info/__manifest__.py
+++ hr_skills_hide_info/__manifest__.py
@@ -2,15 +2,16 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Hr skills hide info",
-    'version': '14.0.1.0.0',
+    "version": "14.0.1.0.0",
     "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/hr-addons",
     "category": "Sales/CRM",
     "depends": [
-        "hr_skills"
+        "hr_skills",
     ],
     "data": [
-        "views/hr_employee_views.xml"
+        "views/hr_employee_views.xml",
     ],
     "license": "AGPL-3",
-    'installable': True,
+    "installable": True,
 }
