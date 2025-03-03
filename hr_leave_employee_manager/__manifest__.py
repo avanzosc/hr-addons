7d93f4254067fedc218422419029d5d5ef5f1438
Unai Beristain <unaiberis@gmail.com>
2025-03-03 12:01:23 +0100

    [ADD] hr_leave_employee_manager: Add domain

---
+++ hr_leave_employee_manager/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "HR Leave Employee Manager",
+    "version": "14.0.1.0.0",
+    "category": "Human Resources",
+    "author": "Avanzosc",
+    "license": "LGPL-3",
+    "depends": ["hr_holidays"],
+    "data": [
+        "views/hr_leave_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "website": "https://github.com/avanzosc/hr-addons",
+}
