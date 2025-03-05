5303b2af7e80f68a676cd26a92305c50dd677d73
Unai Beristain <unaiberis@gmail.com>
2025-03-05 12:44:40 +0100

    [ADD] hr_leave_unusual_days_holidays: Add holidays to unusual days

---
+++ hr_leave_unusual_days_holidays/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "HR Leave Unusual Days Holidays",
+    "version": "16.0.1.1.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/hr-addons",
+    "depends": ["hr"],
+    "data": [],
+    "assets": {
+        "web.assets_backend": ["hr_leave_unusual_days_holidays/static/src/js/**/*"]
+    },
+    "installable": True,
+    "application": False,
+}
