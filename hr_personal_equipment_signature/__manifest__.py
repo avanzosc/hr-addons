911be537198b90b92dae280057751b9f45b26e8e
Ane <aneavanzosc@gmail.com>
2025-06-17 13:34:05 +0200

    [IMP] hr_personal_equipment_signature: Prevents accepting requests without signature

--- hr_personal_equipment_signature/__manifest__.py
+++ hr_personal_equipment_signature/__manifest__.py
@@ -2,11 +2,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Hr Personal Equipment Signature",
-    "version": "14.0.1.0.0",
+    "version": "14.0.2.0.0",
     "category": "Human Resources",
     "summary": "Adds a signature field to personal equipment request",
     "author": "AvanzOSC",
-    "depends": ["hr_personal_equipment_request", "web_digital_sign", "website"],
+    "depends": [
+        "hr_personal_equipment_request",
+        "web_digital_sign",
+        "website",
+    ],
     "data": [
         "views/hr_personal_equipment_signature_view.xml",
     ],
