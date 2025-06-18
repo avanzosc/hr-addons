4361a4ddf29e51c53efa131221b15c417134fe15
Ane <aneavanzosc@gmail.com>
2025-06-18 14:09:22 +0200

    Adjust view to display estimated sale amount

--- hr_employee_estimated_sale/models/hr_employee.py
+++ hr_employee_estimated_sale/models/hr_employee.py
@@ -1,9 +1,9 @@
-from odoo import fields, models
-
+from odoo import models, fields
 
 class HrEmployee(models.Model):
-    _inherit = "hr.employee"
+    _inherit = 'hr.employee'
 
     estimated_sale_price = fields.Monetary(
-        string="Precio estimado venta", currency_field="currency_id"
+        string='Precio estimado venta',
+        currency_field='currency_id'
     )
