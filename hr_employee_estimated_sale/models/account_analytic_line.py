8a3981f11edb2c754c040c29e550658d5b6aaa7a
Ane <aneavanzosc@gmail.com>
2025-06-18 14:16:03 +0200

    Adjust view to display estimated sale amount

--- hr_employee_estimated_sale/models/account_analytic_line.py
+++ hr_employee_estimated_sale/models/account_analytic_line.py
@@ -1,16 +1,19 @@
-from odoo import models, fields, api
+from odoo import api, fields, models
+
 
 class AnalyticLine(models.Model):
-    _inherit='account.analytic.line'
+    _inherit = "account.analytic.line"
 
-    estimated_sale_amount=fields.Monetary(string='Importe estimado venta',
-                                       compute='_compute_estimated_sale_amount',currency_field='currency_id',
-                                       store=True)
-    
-    @api.depends('unit_amount','employee_id.estimated_sale_price')
+    estimated_sale_amount = fields.Monetary(
+        string="Importe estimado venta",
+        compute="_compute_estimated_sale_amount",
+        currency_field="currency_id",
+        store=True,
+    )
 
+    @api.depends("unit_amount", "employee_id.estimated_sale_price")
     def _compute_estimated_sale_amount(self):
         for line in self:
-            price= line.employee_id.estimated_sale_price or 0.0
-            qty=line.unit_amount or 0.0
-            line.estimated_sale_amount= price * qty
+            price = line.employee_id.estimated_sale_price or 0.0
+            qty = line.unit_amount or 0.0
+            line.estimated_sale_amount = price * qty
