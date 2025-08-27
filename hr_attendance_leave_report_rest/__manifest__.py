0fb1fafdda2b47a560d8d5a382129ca1ec2aa804
Ane <aneavanzosc@gmail.com>
2025-08-27 12:09:24 +0200

    [18.0][MIG] hr_attendance_leave_report_rest: Migration to 18.0

---
+++ hr_attendance_leave_report_rest/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2025 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Hr Attendance Leave Report Rest",
+    "version": "18.0.1.1.0",
+    "category": "Human Resources/Attendances",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/hr-addons",
+    "depends": ["hr_attendance_leave_report", "hr_attendance_reason_rest"],
+    "data": ["views/hr_attendance_leave_views.xml"],
+    "installable": True,
+}
