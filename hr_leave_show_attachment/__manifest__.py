9d6fc72d1da78dc699013ea2c6e8e9cdbe3a9464
Alfredo <alfredodelafuente@avanzosc.es>
2025-08-29 08:45:09 +0200

    [18.0][MIG] hr_leave_show_attachment: Migration to 18.0

--- hr_leave_show_attachment/__manifest__.py
+++ hr_leave_show_attachment/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Hr Leave Show Attachment",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Human Resources/Time Off",
     "license": "AGPL-3",
     "author": "AvanzOSC",
