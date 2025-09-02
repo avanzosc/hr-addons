22f493cc683a21cc52c8961c19dc72fb3dd9be1a
Ane <aneavanzosc@gmail.com>
2025-09-02 08:25:06 +0200

    [18.0][MIG] hr_attendance_leave_report: Migration to 18.0

--- hr_attendance_leave_report/__init__.py
+++ hr_attendance_leave_report/__init__.py
@@ -2,12 +2,16 @@ from . import models
 from odoo import api, SUPERUSER_ID
 
 
-def _post_install_put_dates_without_hour(cr, registry):
+def _post_install_put_dates_without_hour(env):
     """
     This method will set the production cost on already done manufacturing orders.
     """
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    admin_user = env.ref("base.user_admin")
+    # antes: env = api.Environment(cr, SUPERUSER_ID, {})
+    try:
+        admin_user = env.ref("base.user_admin")
+    except ValueError:
+        admin_user = env.ref("base.user_root")
+
     env["hr.attendance"].with_user(admin_user)._put_dates_without_hour()
     env["resource.calendar.leaves"].with_user(admin_user)._put_dates_without_hour()
     env["hr.leave"].with_user(admin_user)._put_dates_without_hour()
