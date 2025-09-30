2a536db9cefb8ee4ba6a1a9c7c5ca82f89c43cf0
Ane <aneavanzosc@gmail.com>
2025-09-30 10:48:22 +0200

    [14.0][ADD] hr_attendance_auto_goodbye: close the "More than 12 hours" warning message automatically

---
+++ hr_attendance_auto_goodbye/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Ane Gurruchaga - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "HR Attendance Auto Goodbye",
+    "version": "14.0.1.0.1",
+    "category": "Human Resources/Attendance",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/hr-addons",
+    "depends": ["web", "hr_attendance"],
+    "data": [
+        "views/assets.xml",
+    ],
+    "installable": True,
+}
