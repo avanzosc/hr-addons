267206b44d398a9c7d7153627e24b941bbe45645
Ane <aneavanzosc@gmail.com>
2025-10-03 13:23:37 +0200

    fix: check reasons in hr.attendance.reason and require only if configured

--- hr_attendance_reason_custom/__manifest__.py
+++ hr_attendance_reason_custom/__manifest__.py
@@ -5,13 +5,9 @@
     "author": "Avanzosc",
     "license": "LGPL-3",
     "depends": [
+        "hr_attendance",
         "hr_attendance_reason",
     ],
-    "assets": {
-        "web.assets_backend": [
-            "hr_attendance_reason_custom/static/src/js/**/*",
-        ],
-    },
     "data": [],
     "installable": True,
     "application": False,
