1b9558c72cf3b4f0b88d6298e2d36b6e0719cf3a
Alfredo <alfredodelafuente@avanzosc.es>
2025-10-09 11:31:10 +0200

    [16.0][ADD] hr_recruitment_attachment_show_tree_view: In "ir.attachment" tree view new button "Show" for view attachment.

---
+++ hr_recruitment_attachment_show_tree_view/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2025 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Hr Recruitment Attachment Show Tree View",
+    "version": "16.0.1.1.0",
+    "category": "Manufacturing/Manufacturing",
+    "website": "https://github.com/avanzosc/hr-addons",
+    "author": "AvanzOSC",
+    "license": "AGPL-3",
+    "depends": ["hr_recruitment", "ir_attachment_show_tree_view"],
+    "data": [
+        "views/ir_attachment_views.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
