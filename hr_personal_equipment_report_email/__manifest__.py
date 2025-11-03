080a46494169940cf230b8a45875a775c7e79719
luciaavanzosc <luciaavanzosc@gmail.com>
2025-11-03 11:38:28 +0100

    [14.0][ADD] hr_personal_equipment_report_email: New report and mail template for personal equipment requests.

--- hr_personal_equipment_report_email/__manifest__.py
+++ hr_personal_equipment_report_email/__manifest__.py
@@ -2,20 +2,16 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Hr Personal Equipment Report Email",
-    "summary":"New report and mail template for personal equipment requests.",
+    "summary": "New report and mail template for personal equipment requests.",
     "version": "14.0.1.0.0",
     "category": "",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/hr-addons",
-    "depends": [
-        'stock_picking_date_done',
-        'hr_personal_equipment_signature'
-    ],
+    "depends": ["stock_picking_date_done", "hr_personal_equipment_signature"],
     "data": [
-        'report/personal_equipment_report.xml',
-        'data/email_template.xml',
+        "report/personal_equipment_report.xml",
+        "data/email_template.xml",
     ],
     "installable": True,
 }
-
