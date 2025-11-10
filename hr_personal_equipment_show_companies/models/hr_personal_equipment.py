78e1526bc516f90b5fb439f7e29c9de3feb4ae3c
luciaavanzosc <luciaavanzosc@gmail.com>
2025-11-10 10:50:25 +0100

    [14.0][ADD] hr_personal_equipment_show_companies: Show employee and product companies on PPE Request assignments.

---
+++ hr_personal_equipment_show_companies/models/hr_personal_equipment.py
@@ -0,0 +1,18 @@
+# Copyright 2025 Lucía Echeverría - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import models, fields, api
+
+class HrPersonalEquipment(models.Model):
+    _inherit = 'hr.personal.equipment'
+
+    company_id_employee = fields.Many2one(
+        string='Employee Company',
+        related='employee_id.company_id',
+        readonly=True
+    )
+
+    company_ids_product = fields.Many2many(
+        string='Product Companies',
+        related='product_id.company_ids',
+        readonly=True
+    )
