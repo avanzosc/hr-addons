9c4fe4ec5b84b67298e6813e262a188765ebb984
luciaavanzosc <luciaavanzosc@gmail.com>
2025-11-10 10:54:15 +0100

    [14.0][ADD] hr_personal_equipment_show_companies: Show employee and product companies on PPE Request assignments.

--- hr_personal_equipment_show_companies/__manifest__.py
+++ hr_personal_equipment_show_companies/__manifest__.py
@@ -2,19 +2,16 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "HR Personal Equipment Show Companies",
-    "summary":"Show employee and product companies on PPE Request assignments.",
+    "summary": "Show employee and product companies on PPE Request assignments.",
     "version": "14.0.1.0.0",
     "category": "Hr",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/hr-addons",
     "depends": [
-        'hr_personal_equipment_request',
-        'product_multi_company_usability',
+        "hr_personal_equipment_request",
+        "product_multi_company_usability",
     ],
-    "data": [
-        'views/hr_personal_equipment_views.xml'
-    ],
+    "data": ["views/hr_personal_equipment_views.xml"],
     "installable": True,
 }
-
--- hr_personal_equipment_show_companies/models/hr_personal_equipment.py
+++ hr_personal_equipment_show_companies/models/hr_personal_equipment.py
@@ -1,18 +1,15 @@
 # Copyright 2025 Lucía Echeverría - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models, fields, api
+from odoo import fields, models
+
 
 class HrPersonalEquipment(models.Model):
-    _inherit = 'hr.personal.equipment'
+    _inherit = "hr.personal.equipment"
 
     company_id_employee = fields.Many2one(
-        string='Employee Company',
-        related='employee_id.company_id',
-        readonly=True
+        string="Employee Company", related="employee_id.company_id", readonly=True
     )
 
     company_ids_product = fields.Many2many(
-        string='Product Companies',
-        related='product_id.company_ids',
-        readonly=True
+        string="Product Companies", related="product_id.company_ids", readonly=True
     )
