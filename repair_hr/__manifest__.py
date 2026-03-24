6d5e5d8fac23e11c3743399a6e093f79bcb54631
luciaavanzosc <luciaavanzosc@gmail.com>
2026-03-24 08:23:33 +0100

    [18.0][MIG] repair_hr: Migration to 18.0.

--- repair_hr/__manifest__.py
+++ repair_hr/__manifest__.py
@@ -1,8 +1,8 @@
 # Copyright 2024 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    "name": "HR Stock Picking Create Repair",
-    "version": "14.0.1.0.0",
+    "name": "Repair HR",
+    "version": "18.0.1.0.0",
     "category": "Human Resources/Employees",
     "license": "AGPL-3",
     "author": "Avanzosc",
