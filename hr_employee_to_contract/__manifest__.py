9101404a0414070917603a78993a634b7c5f116a
Berezi Amubieta <bereziamubieta@avancosc.es>
2026-04-21 15:09:56 +0200

    [14.0][ADD] hr_employee_to_contract:

---
+++ hr_employee_to_contract/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2026 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "HR Employee to Contract",
+    "version": "14.0.1.0.0",
+    "category": "Human Resources/Employees",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/hr-addons",
+    "depends": [
+        "hr",
+        "hr_contract",
+        "hr_employee_sees_himself",
+    ],
+    "data": [
+        "views/hr_employee_view.xml",
+    ],
+    "installable": True,
+}
