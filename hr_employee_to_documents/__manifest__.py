0f5d9dd698c583d4a1d309eebdbd0c8692aa0254
aneravanzosc <aneravanzosc@gmail.com>
2026-07-20 10:27:14 +0200

    [18.0][MIG] hr_employee_to_documents: Migration to 18.0

--- hr_employee_to_documents/__manifest__.py
+++ hr_employee_to_documents/__manifest__.py
@@ -3,14 +3,14 @@
 
 {
     "name": "HR Employee to Documents",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Human Resources/Employees",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/hr-addons",
     "depends": [
         "hr",
-        "oh_employee_documents_expiry",
+        "employee_documents_expiry",
         "hr_employee_sees_himself",
     ],
     "data": [
